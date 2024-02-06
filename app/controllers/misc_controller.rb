6c1cb5546b155f9742b149e81b7ee9e552a51ff8
Cisco Vlahakis <ciscovlahakis@gmail.com>
2024-02-06 06:11:46 +0000

    finished

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,40 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+  def directors
+    @directors = Director.all
+    render({ :template => "misc_templates/directors"})
+  end
+  def show_director
+    @director = Director.find(params[:id])
+    @movies = @director.movies
+    render({ :template => "misc_templates/show_director" })
+  end
+  def directors_youngest
+    @youngest_director = Director.where.not(dob: nil).order(dob: :desc).first
+    render({ :template => "misc_templates/directors/youngest"})
+  end
+  def directors_eldest
+    @eldest_director = Director.where.not(dob: nil).order(dob: :asc).first
+    render({ :template => "misc_templates/directors/eldest"})
+  end
+  def movies
+    @movies = Movie.includes(:director).all
+    render({ :template => "misc_templates/movies"})
+  end
+  def show_movie
+    @movie = Movie.find(params[:id])
+    render({ :template => "misc_templates/show_movie" })
+  end
+  def actors
+    @actors = Actor.all
+    render({ :template => "misc_templates/actors"})
+  end
+  def show_actor
+    @actor = Actor.find(params[:id])
+    @characters = @actor.characters.includes(:movie)
+    # To collect all unique movies where the actor has a character
+    @movies = @characters.map(&:movie).uniq
+    render({ :template => "misc_templates/show_actor" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "misc", :action => "directors" })
+  get("/directors/youngest", { :controller => "misc", :action => "directors_youngest" })
+  get("/directors/eldest", { :controller => "misc", :action => "directors_eldest" })
+  get "/directors/:id", to: "misc#show_director", as: "director"
+  get("/movies", { :controller => "misc", :action => "movies" })
+  get "/movies/:id", to: "misc#show_movie", as: "movie"
+  get("/actors", { :controller => "misc", :action => "actors" })
+  get "/actors/:id", to: "misc#show_actor", as: "actor"
 end
